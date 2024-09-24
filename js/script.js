a2754843cd78dc95d09a179fc6722c861010d642
Ryusaem <53605044+Ryusaem@users.noreply.github.com>
2024-09-24 20:29:58 +0200

    Generating the reply container

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // --- VARIABLES ---
-const commentReply = document.querySelectorAll(".comment__reply");
+const commentReplies = document.querySelectorAll(".comment__reply");
 
 // ----------------------------------------------
 
@@ -8,23 +8,46 @@ function clickReply() {
   console.log("clicked");
 }
 
+function createForm(parents) {
+  // Check if reply form already exists
+  if (parents.querySelector("reply-container")) return;
+
+  const replyForm = document.createElement("div");
+  replyForm.classList.add("reply-container");
+
+  replyForm.innerHTML = `
+
+    <textarea class="reply__form" placeholder="Add a comment..."></textarea>
+
+    <img src="./images/avatars/image-amyrobson.png" class="comment__avatar reply__avatar" alt="avatar"></img>
+
+    <button type="submit" class="reply__submit-btn">REPLY</button>
+
+`;
+
+  // Append the reply form to the bottom of the comment container
+  // parents.appendChild(replyForm);
+  parents.insertAdjacentElement("afterend", replyForm);
+}
+
+function createReply(parents) {
+  const replyContainer = document.createElement("div");
+  replyContainer.className = "answer-container";
+
+  replyContainer.innerHTML = `<div class="border"></div>
+    <div class="comment-container answer"></div>`;
+
+  parents.insertAdjacentElement("afterend", replyContainer);
+}
+
 // ----------------------------------------------
 
 // --- EVENT LISTENERS ---
-commentReply.forEach((reply) => {
+commentReplies.forEach((reply) => {
   reply.addEventListener("click", (event) => {
     const parentComment = event.target.closest(".comment-container");
 
-    // Assuming replyComment is the new element you want to insert
-    const replyComment = document.createElement("div");
-    replyComment.className = "answer-container";
-    // replyComment.textContent = "This is a reply comment";
-
-    // Insert the new element after the reference element
-    parentComment.insertAdjacentElement("afterend", replyComment);
-
-    replyComment.innerHTML = `<div class="border"></div>
-    <div class="comment-container answer"></div>`;
+    createForm(parentComment);
   });
 });
 
